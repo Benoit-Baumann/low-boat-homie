38f41e4564cad34773ca56abd4a83784850397a2
Benoit Baumann <baumann.benoit@gmail.om>
2019-11-12 18:39:24 +0100

    added boat_policy.rb for pundit

---
+++ app/policies/boat_policy.rb
@@ -0,0 +1,24 @@
+class BoatPolicy < ApplicationPolicy
+    
+    def create?
+        true
+      end
+    
+      def show?
+        true
+      end
+    
+      def destroy?
+        record.user == user
+      end
+    
+      def update?
+        record.user == user
+      end
+    
+      class Scope < Scope
+        def resolve
+          scope.all
+        end
+    end
+end
