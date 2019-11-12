af1e18f9c92ede70692e2948f010baed37c7a71e
Benoit Baumann <baumann.benoit@gmail.om>
2019-11-12 12:14:33 +0100

    routes defined

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+
+  resources :boats do
+    ressouces :reviews, only: [:new, :create, :edit, :update, :destroy]
+  end
+
+  resources :users, only: [:show, :new, :create, :edit, :update, :destroy] do
+    collection do
+        get :my_boats
+        get :my_rentals
+    end
+  end  
+
+  root to: 'boats#search'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
