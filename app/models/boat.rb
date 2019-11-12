4fcc2ff6f65318290fcea81da750523b14fa31a9
Benoit Baumann <baumann.benoit@gmail.om>
2019-11-12 12:07:41 +0100

    Validation on boat model added

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,8 @@ class Boat < ApplicationRecord
   belongs_to :owner, class_name: 'User'
   has_many :reviews
   has_many :rentals
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :description, length: { in: 25..1000 }
+  validates :type, presence: true
 end
