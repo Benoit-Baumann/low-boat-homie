803c038ff87c0330605c4e3b132875cd7107735c
John Jomard <john-mackenzie.jomard@mail.mcgill.ca>
2019-11-13 12:33:04 +0100

    final v of seed and migrate. Ne plus toucher

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -5,6 +5,6 @@ class Boat < ApplicationRecord
   has_many :rentals
   validates :name, presence: true
   validates :location, presence: true
-  validates :description, length: { in: 25..1000 }
-  validates :type, presence: true
+  validates :description, length: { in: 10..1000 }
+  validates :category, presence: true
 end
