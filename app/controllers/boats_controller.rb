a9372939e43a914f0c4708994d6431fea8e0c15c
Benoit Baumann <baumann.benoit@gmail.om>
2019-11-14 12:38:32 +0100

    search bar optimized with form_tag

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -2,14 +2,11 @@ class BoatsController < ApplicationController
 
     # GET /boats
     def index
-        if params[:search].nil?
-            #Requis pour éviter erreur Pundit "Pundit::PolicyScopingNotPerformedError in BoatsController#index"
-            #Skiper pundit pour la méthode index et search ???
-            @boats = policy_scope(Boat)
-            redirect_to root_path
-        else
-            @query = params[:search][:query]
-            @boats = policy_scope(Boat).near(@query, 20)
+        if params[:city].present?
+            @city = params[:city].capitalize
+            @distance = params[:distance].to_i
+
+            @boats = policy_scope(Boat).near(@city, @distance)
 
             #.geocoded nécessaire d'après cours mais résultats similaires sans
             # @boats_geo = policy_scope(Boat).near(@query, 20).geocoded
@@ -21,6 +18,11 @@ class BoatsController < ApplicationController
                     infoWindow: render_to_string(partial: "info_window", locals: { boat: boat })
                 }
             end
+        else 
+            #Requis pour éviter erreur Pundit "Pundit::PolicyScopingNotPerformedError in BoatsController#index"
+            #Skiper pundit pour la méthode index et search ???
+            @boats = policy_scope(Boat)
+            redirect_to root_path
         end
     end
 
@@ -33,7 +35,6 @@ class BoatsController < ApplicationController
 
     #GET /boats
     def search
-        #To be modified later
         @boats = policy_scope(Boat)
         # authorize @boats
     end
